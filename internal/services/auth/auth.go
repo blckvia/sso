e9778eaa9eb2c8abeec905c217de8cd2f44df933
blckvia <a.vyatkinn@icloud.com>
2023-12-27 15:07:16 +0300

    feat: add Login method & hash the password

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -2,11 +2,13 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"golang.org/x/crypto/bcrypt"
 	"log/slog"
 	"sso/internal/domain/models"
 	"sso/internal/lib/logger/sl"
+	"sso/internal/storage"
 	"time"
 )
 
@@ -35,6 +37,10 @@ type AppProvider interface {
 	App(ctx *context.Context, appID int) (models.App, error)
 }
 
+var (
+	ErrInvalidCredentials = errors.New("invalid credentials")
+)
+
 // New return new auth service instance
 func New(
 	log *slog.Logger,
@@ -57,12 +63,45 @@ func New(
 // If user exists, but password is incorrect, returns error.
 // If user doesn't exist, returns error.
 func (a *Auth) Login(
-	ctx context.Context,
+	ctx *context.Context,
 	email string,
 	password string,
 	appID int,
 ) (string, error) {
-	panic("implement me")
+	const op = "auth.Login"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("username", email),
+	)
+
+	log.Info("attempting to login user")
+
+	user, err := a.usrProvider.User(ctx, email)
+	if err != nil {
+		if errors.Is(err, storage.ErrUserNotFound) {
+			a.log.Warn("user not found", sl.Err(err))
+
+			return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+		}
+
+		a.log.Error("failed to get user", sl.Err(err))
+
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.PassHash, []byte(password)); err != nil {
+		a.log.Info("invalid password", sl.Err(err))
+
+		return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+	}
+
+	app, err := a.appProvider.App(ctx, appID)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	a.log.Info("user logged in successfully")
 }
 
 // RegisterNewUser registers new user in the system and returns user ID.
